6b72d59760ae1f0646a7e35d5062c8057e7c0bf5
Aman Arya <aman.arya@globallogic.com>
2021-10-04 16:22:59 +0530

    Modular bank assignment

---
+++ src/test/java/com/modular/bank/test/ModularbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modular.bank.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
